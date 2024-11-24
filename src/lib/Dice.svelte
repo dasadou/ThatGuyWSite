7dad26b103758e38c909b36bd4bb03d070005970
dasadou <65833938+dasadou@users.noreply.github.com>
2024-11-23 22:18:29 -0500

    Rolling dice, no animations

--- src/lib/Dice.svelte
+++ src/lib/Dice.svelte
@@ -1,7 +1,11 @@
 ﻿<script lang="ts">
-    import {T} from '@threlte/core';
-    import {OrbitControls} from "@threlte/extras";
+    import {T, useThrelte} from '@threlte/core';
+    import {interactivity, TrackballControls} from "@threlte/extras";
     import {BoxGeometry, CanvasTexture, MeshBasicMaterial} from "three";
+    import {get} from "svelte/store";
+
+    export let rotateSpeed: number = 10;
+    interactivity();
 
     function createTextTexture(text: string) {
         const canvas = document.createElement('canvas');
@@ -29,23 +33,60 @@
 
     // Create the materials for the cube
     const materials = textures.map(texture => new MeshBasicMaterial({map: texture}));
-</script>
 
-<!-- face.materialIndex = face du de
- ou bien -->
-<T.Mesh position.y={0} material={materials} onclick={(ref:any) => {
+    function getRandomRotation() {
+        // Generate random rotation angles 
+        const x = Math.random() * 2 * Math.PI;
+        const y = Math.random() * 2 * Math.PI;
+        const z = Math.random() * 2 * Math.PI;
+        return {x, y, z};
+    }
+
+    let cube: any;
+    const {scene, invalidate} = useThrelte();
+
+    function onDiceClick() {
+        // console.log('camera');
+        // console.log(camera);
+        // camera.lookAt(0, 0, 0);
+        // if(cube){
+        console.log('onDiceClick1');
+        console.log(cube);
+        console.log(typeof cube);
+        const randomRotation = getRandomRotation();
+        console.log('randomRotation', randomRotation);
+        console.log("rotation: ", cube.rotation);
+        cube.rotation.set(randomRotation.x, randomRotation.y, randomRotation.z);
+        // }
+
+    }
+
+</script>
+<T.GridHelper args={[10, 10]}/>
+
+<!-- face.materialIndex = face du de -->
+<T.Mesh
+        onpointerover={(e)=>{
+            const randomRot = getRandomRotation();
+            e.object.rotation.set(randomRot.x,randomRot.y,randomRot.z,"XYZ");
+        }}
+        position.y={0.5}
+        material={materials}
+        onclick={(ref:any) => {
     console.log(ref.intersections[0].face.materialIndex + 1);
+    onDiceClick();
   }}>
-    <T.BoxGeometry args={[10, 10, 10]}/>
+    <T.BoxGeometry args={[1, 1, 1]}/>
 </T.Mesh>
 
 <T.PerspectiveCamera
+        bind:this={camera}
         makeDefault
-        position={[10, 10, 10]}
+        position={[1, 1, 1]}
         oncreate={(ref) => {
     ref.lookAt(0, 0, 0);
   }}
 >
-    <OrbitControls !autoRotate></OrbitControls>
+    <TrackballControls {rotateSpeed}></TrackballControls>
 </T.PerspectiveCamera>
 
